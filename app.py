c221ec6ebc703c88495dc6b84952ee3bcd4a4171
ubcuas <info@ubcuas.com>
2019-09-24 02:52:38 +0000

    Resolve "fix sitl"

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def setup_mavlink_connection():
     global mavlink_connection
     if mavlink_connection == None or mavlink_connection.target_system < 1:
         app.logger.info("Mavlink connection is now being initialized")
-        mavlink_connection = mavutil.mavlink_connection('tcp:localhost:5760')
+        mavlink_connection = mavutil.mavlink_connection('tcp:172.18.0.3:5760')
         mavlink_connection.wait_heartbeat(timeout=3)
         app.logger.info("Heartbeat from system (system %u component %u)" % (mavlink_connection.target_system, mavlink_connection.target_system))
     if mavlink_connection.target_system < 1:
@@ -21,8 +21,10 @@ def setup_mavlink_connection():
 @app.route('/aircraft/reroute', methods=['POST'])
 def aircraft_reroute():
     global mavlink_connection
-    app.logger.info(mavlink_connection.waypoint_current())
-    waypoints = request.data
+    testRetr = mavlink_connection.recv_match()
+    if testRetr != None:
+        app.logger.info(testRetr.to_dict())
+    # waypoints = request.data
 
     return request.data
     
