ad7a59977a4f26f8b693d0aa710992e42386d866
Johnson Vu <johnsonvu96@gmail.com>
2019-10-17 22:19:06 -0700

    Fix style

---
+++ library/mavlink_messages.py
@@ -0,0 +1,24 @@
+import xmltodict
+
+# A class that parses the Mavlink messages definitions
+# Source: https://raw.githubusercontent.com/mavlink/mavlink/master/message_definitions/v1.0/common.xml
+class MavlinkMessage:
+    mavlink_messages = {}
+
+    def __init__(self):
+        with open('../resources/mavlink_messages_v1.0.xml', 'r') as fd:
+            self.mavlink_messages = xmltodict.parse(fd.read())
+
+    # returns the attributes of the message of interest
+    # Example: get_message_attr('GPS_RAW_INT') -> ['time_usec', 'fix_type', 'lat', 'lon', 'alt' ...]
+    def get_message_attr(self, message_name):
+        messages = self.mavlink_messages['mavlink']['messages']['message']
+        result_list = []
+        # iterate through the messages tree
+        for message in messages:
+            if message['@name'] == message_name:
+                # iterate through the fields to retrieve attributes
+                for field in message['field']:
+                    result_list.append(field['@name'])
+
+        return result_list
