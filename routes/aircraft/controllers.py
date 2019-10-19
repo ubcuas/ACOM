aefb914421874dcd1dbc4824a5dbfe68ae906ea4
Johnson Vu <johnsonvu96@gmail.com>
2019-10-18 20:47:10 -0700

    Add basic mavlink command eps

--- routes/aircraft/controllers.py
+++ routes/aircraft/controllers.py
@@ -30,19 +30,64 @@ def aircraft_reroute():
     return request.data
 
 # Changes the flight mode of the aircraft
-@aircraft.route('/flightmode', methods=['POST'])
-def aircraft_flightmode():
-    return None
+@aircraft.route('/flightmode/<mav_mode>', methods=['POST'])
+def aircraft_flightmode(mav_mode):
+    global mavlink_connection
+    mavlink_connection.mav.command_long_send(
+        mavlink_connection.target_system,
+        mavlink_connection.target_component,
+        mavutil.mavlink.MAV_CMD_DO_SET_MODE,
+        int(mav_mode),
+        0, 0, 0, 0, 0, 0, 0  # unused parameters
+    )
+    return aircraft_telemetry('HEARTBEAT')
 
-# Arms or disarms the aircraft
-@aircraft.route('/arm', methods=['POST'])
+# Arms the aircraft
+@aircraft.route('/arm', methods=['PUT'])
 def aircraft_arm():
-    return None
+    global mavlink_connection
+    mavlink_connection.mav.command_long_send(
+        mavlink_connection.target_system,
+        mavlink_connection.target_component,
+        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+        0, # confirmation
+        1, # arm
+        0, 0, 0, 0, 0, 0  # unused parameters
+    )
+    return aircraft_telemetry('HEARTBEAT')
 
-# Manual control
+# Disarms the aircraft
+@aircraft.route('/disarm', methods=['PUT'])
+def aircraft_disarm():
+    global mavlink_connection
+    mavlink_connection.mav.command_long_send(
+        mavlink_connection.target_system,
+        mavlink_connection.target_component,
+        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+        0, # confirmation
+        0, # disarm
+        0, 0, 0, 0, 0, 0 # unused parameters
+    )
+    return aircraft_telemetry('HEARTBEAT')
+
+# Manual control / Fly-to
 @aircraft.route('/manual', methods=['POST'])
 def aircraft_manual():
-    return None
+    global mavlink_connection
+    mavlink_connection.mav.command_long_send(
+        mavlink_connection.target_system,
+        mavlink_connection.target_component,
+        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+        request.json['hold'],
+        request.json['accept_radius'],
+        request.json['pass_radius'],
+        request.json['yaw'],
+        request.json['lat'],
+        request.json['lon'],
+        request.json['alt'],
+        0
+    )
+    return aircraft_telemetry('GPS_RAW_INT')
 
 # Request telemetry data based on mavlink message names
 # Example: /aircraft/telemetry/GPS_RAW_INT
