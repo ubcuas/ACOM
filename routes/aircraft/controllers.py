cd5aa62b0da41afd1c93ead777331db214faa21a
Johnson Vu <johnsonvu96@gmail.com>
2019-10-19 18:33:14 +0000

    Add mavlink message requests + translation

--- routes/aircraft/controllers.py
+++ routes/aircraft/controllers.py
@@ -1,31 +1,32 @@
 from flask import Blueprint, request, jsonify, abort, Response, current_app
 from pymavlink import mavutil
+from library import mavlink_messages
+import json
 import logging
 
 aircraft = Blueprint('aircraft', __name__)
 mavlink_connection = None
+mavlink_msg_dict = mavlink_messages.MavlinkMessage()
+debug = False # Set to True in order to bypass authentication and mavlink connection
 
 # Ensure mavlink connection is created before sending requests
 @aircraft.before_request
 def setup_mavlink_connection():
     global mavlink_connection
-    if mavlink_connection == None or mavlink_connection.target_system < 1:
-        current_app.logger.info("Mavlink connection is now being initialized")
-        mavlink_connection = mavutil.mavlink_connection('tcp:172.18.0.3:5760')
-        mavlink_connection.wait_heartbeat(timeout=3)
-        current_app.logger.info("Heartbeat from system (system %u component %u)" % (mavlink_connection.target_system, mavlink_connection.target_system))
-    if mavlink_connection.target_system < 1:
-        abort(400, "Mavlink is not connected")
+    if not debug:
+        # TODO: Add a check to make sure connection is valid
+        if mavlink_connection == None or mavlink_connection.target_system < 1:
+            current_app.logger.info("Mavlink connection is now being initialized")
+            mavlink_connection = mavutil.mavlink_connection('udp:127.0.0.1:14550')
+            mavlink_connection.wait_heartbeat(timeout=3)
+            current_app.logger.info("Heartbeat from system (system %u component %u)" % (mavlink_connection.target_system, mavlink_connection.target_system))
+        if mavlink_connection.target_system < 1:
+            abort(400, "Mavlink is not connected")
 
 # Recieves an array of waypoints
 # Interrupts aircraft auto mode, switches to guided, runs waypoints, then returns to auto
 @aircraft.route('/reroute', methods=['POST'])
 def aircraft_reroute():
-    global mavlink_connection
-    testRetr = mavlink_connection.recv_match()
-    if testRetr != None:
-        current_app.logger.info(testRetr.to_dict())
-    # waypoints = request.data
     return request.data
 
 # Changes the flight mode of the aircraft
@@ -43,7 +44,23 @@ def aircraft_arm():
 def aircraft_manual():
     return None
 
-# Get data
-@aircraft.route('/telemetry', methods=['GET'])
-def aircraft_telemetry():
-    return None
+# Request telemetry data based on mavlink message names
+# Example: /aircraft/telemetry/GPS_RAW_INT
+@aircraft.route('/telemetry/<message_name>', methods=['GET'])
+def aircraft_telemetry(message_name):
+    global mavlink_connection
+    msg = mavlink_connection.recv_match(type=message_name)
+    msg_data = {}
+
+    if not msg:
+        return {'error': 'Invalid message name'}
+    if msg.get_type() == "BAD_DATA":
+        return {'error': 'Bad data retrieved'}
+    else:
+        attributes = mavlink_msg_dict.get_message_attr(message_name)
+        for attr in attributes:
+            current_app.logger.info("The attr %s val is %s", attr, getattr(msg,attr))
+            msg_data[attr] = getattr(msg, attr)
+
+    # jsonify msg_data
+    return json.dumps(msg_data)
